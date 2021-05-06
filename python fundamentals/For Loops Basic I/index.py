e45beff11e38fc0a7b5353bee34de17794f15f66
alaa <adeenib@gmail.com>
2021-05-06 09:10:02 +0300

    first

---
+++ python fundamentals/For Loops Basic I/index.py
@@ -0,0 +1,26 @@
+# for x in range(150):
+#     print(x)
+# for x in range(1, 1000, +5):
+#     print(x)
+# for x in range(0, 100):
+#     if x % 10 == 0:
+#         print("Coding Dojo")
+#     elif x % 5 == 0:
+#         print("Coding")
+#     else:
+#         print(x)
+
+# x = 0
+# for i in range(500000):
+#     if i % 2 != 0:
+#         x = x+i
+# print(x)
+
+# for x in range(2018, 0, -4):
+#     print(x)
+# lowNum = 2
+# highNum = 9
+# mult = 3
+# for i in range(lowNum, highNum+1):
+#     if i % mult == 0:
+#         print(i)
