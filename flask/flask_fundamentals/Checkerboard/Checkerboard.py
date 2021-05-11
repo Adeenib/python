fc20ec9624f509245bc9fbad88c218f5b1364aab
alaa <adeenib@gmail.com>
2021-05-11 19:40:45 +0300

    day5

--- flask/flask_fundamentals/Checkerboard/Checkerboard.py
+++ flask/flask_fundamentals/Checkerboard/Checkerboard.py
@@ -12,19 +12,16 @@ def eight_by_eight(n=8, y=8):
 
 
 @app.route('/<n>/<y>')
-def num_by_num(n=8, y=8):
-    print(n)
-    print(y)
-    return render_template('Checkerboard.html', x=int(n), y=int(int(y)*0.5))
-
-
 @app.route('/<n>/<y>/<color>/<color1>')
 def num_by_color(n=8, y=8, color='red', color1='black'):
+    import math
     print(n)
     print(y)
     print(color)
     print(color1)
-    return render_template('Checkerboard.html', x=int(n), y=int(int(y)*0.5), color=color, color1=color1)
+    size = int(y)*50
+
+    return render_template('Checkerboard.html', x=int(n), y=math.ceil(int(y)*0.5), color=color, color1=color1, fm=int(y), size=size)
 
 
 if __name__ == "__main__":
